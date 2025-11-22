81bb05475950af835085b5baed72ed41747797d5
Xebec13 <dhoesen@gmail.com>
2025-11-22 21:03:52 +0100

    change hero spacing from between to evenly

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section id="/" className="relative flex md:min-h-screen flex-col items-stretch justify-center gap-10 px-5 pb-10 pt-30 md:justify-between md:px-22 md:pt-35">
+        <section id="/" className="relative flex min-h-screen flex-col items-stretch justify-center md:justify-evenly gap-10 px-5 pb-15 pt-30 md:px-22 md:pt-35">
             {/* Header Section */}
-            <HeroGridBg />
+            {/* <HeroGridBg /> */}
             <div className="relative flex flex-col-reverse justify-center gap-5 md:flex-row md:justify-between -z-10">
                 {/* Large uppercase name */}
                 <div className="max-w-fit text-[clamp(4.25rem,12vw,15rem)] font-semibold uppercase leading-none">
